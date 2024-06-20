48dbdeea1f22be18cc2022711437c85f015ef248
Cathy Garcia <garciacathy67@yahoo.com>
2024-06-20 16:03:29 -0700

    Working on the styling for artist info page.

--- src/ArtistInfo.tsx
+++ src/ArtistInfo.tsx
@@ -12,9 +12,15 @@ export function ArtistInfo() {
   return (
     <>
       <Navbar text="Discover Me" section="Artist Info" />
-      <div className="artist-info">
-        <h1>{artist.name}</h1>
-        <img src={artist.images[0].url} alt={artist.name} />
+      <div className="flex justify-center flex-col items-center mt-12">
+        <h1 className="text-white font-reggae">{artist.name}</h1>
+        <div className="w-2/4 ">
+          <img
+            src={artist.images[0].url}
+            alt={artist.name}
+            className="h-full w-full rounded-3xl"
+          />
+        </div>
         <p>{artist._embedded.venues[0].name}</p>
         <p>{artist.dates.start.localDate}</p>
       </div>
