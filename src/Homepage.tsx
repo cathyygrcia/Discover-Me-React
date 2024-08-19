aeb19dab5b021e854d04efd85cfbdfeeb2dc97c6
Cathy Garcia <garciacathy12@gmail.com>
2024-08-19 12:12:24 -0700

    Working on sharing state between components

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Searchbar from "./Searchbar";
 import "./index.css";
@@ -7,54 +7,31 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function Homepage() {
   const [artists, setArtists] = useState<ArtistProps[]>([]);
+  const [input, setInput] = useState("");
 
-  const dmaArray = [
-    "324",
-    "222",
-    "228",
-    "300",
-    "264",
-    "345",
-    "354",
-    "368",
-    "372",
-    "378",
-    "380",
-    "381",
-    "382",
-    "383",
-    "385",
-  ];
-
-  const randomIndex = Math.floor(Math.random() * dmaArray.length);
-  const randomDMA = dmaArray[randomIndex];
-
-  useEffect(() => {
-    async function getRandom(DMA: string) {
-      try {
-        const res = await fetch(
-          `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=${DMA}&size=30&apikey=aeMvG0zyzdpO1jAkGyCZeGxxQK4vIfpe`
-        );
-        if (!res.ok) throw new Error(`Error: ${res.status}`);
-        const result = await res.json();
-        console.log(result);
-        setArtists(result._embedded.events);
-      } catch (error) {
-        console.error(error);
-      }
+  async function handleSearch() {
+    try {
+      const res = await fetch(
+        `https://app.ticketmaster.com/discovery/v2/attractions.json?classificationName=${input}&size=30&apikey=aeMvG0zyzdpO1jAkGyCZeGxxQK4vIfpe`
+      );
+      if (!res.ok) throw new Error(`Error: ${res.status}`);
+      const result = await res.json();
+      setArtists(result._embedded.attractions || []);
+    } catch (error) {
+      console.error(error);
     }
-    getRandom(randomDMA);
-  }, []);
-
-  function formatDate(inputDate: string) {
-    const date = new Date(inputDate);
-    return date.toLocaleDateString();
   }
 
   return (
     <>
       <Navbar text="Discover Me" section="" />
-      <Searchbar />
+      <Searchbar
+        input={input}
+        setInput={setInput}
+        setArtists={setArtists}
+        handleSearch={handleSearch}
+      />
+
       <div className="flex justify-center items-center flex-wrap">
         {artists.map((artist) => (
           <div
@@ -70,14 +47,7 @@ export default function Homepage() {
                 />
               </Link>
             </div>
-
             <p className="text-white text-base m-0">{artist.name}</p>
-            <p className="text-white text-base m-0">
-              {artist._embedded.venues[0].name}
-            </p>
-            <p className="text-white text-base m-0">
-              {formatDate(artist.dates.start.localDate)}
-            </p>
           </div>
         ))}
       </div>
