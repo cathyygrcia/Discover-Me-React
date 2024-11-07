7771d688116459cd898700d372acb7e4c0786a75
Cathy Garcia <garciacathy12@gmail.com>
2024-11-07 13:25:52 -0800

    Added edits to search bar for mobile version

--- src/Searchbar.tsx
+++ src/Searchbar.tsx
@@ -3,16 +3,17 @@ import React from "react";
 export default function Searchbar({ input, setInput, handleSearch }: any) {
   return (
     <>
-      <div className="flex justify-center items-center  ">
-        <label className=" ">
+      <div className="flex justify-center items-center ">
+        <label className="flex w-4/12">
           <input
             type="text"
-            className="bg-black rounded-2xl p-2 text-white outline-none"
+            className="bg-black rounded-2xl p-4 text-white outline-none w-full"
             placeholder="Search Genre..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
         </label>
+
         <button
           type="button"
           className="rounded-2xl my-4 ml-6 p-3.5 bg-black text-white"
